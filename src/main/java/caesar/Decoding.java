fe17aa699642f17cc313deb92c6d31d0417c5bdb
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 16:55:00 +0300

    add for loop to loop through the indices of the alphabets in the string

--- src/main/java/caesar/Decoding.java
+++ src/main/java/caesar/Decoding.java
@@ -6,6 +6,16 @@ public class Decoding {
         cText =cText.toLowerCase();
         //to generate cipher text that was encoded to plain text
         String theText = "";
+        //loop through index of alphabet to find our characters' indices
+        for (int i = 0; i < theText.length(); i++) {
+            int charIndex = alphabet.indexOf(cText.charAt(i));
+            int newIndex = (charIndex - key) % 26;
+            //add if statement in case we get a negative value from subtraction
+
+
+            }
+        }
+
         return theText;
     }
 
