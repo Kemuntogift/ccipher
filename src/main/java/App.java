a1dfb91cf30b03e593f5d0903095285bf8a9101c
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 17:18:07 +0300

    for user interface import scanner

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
-import caesar.CaesarCipher;
+import caesar.Decoding;
+import caesar.EncodingC;
 
 import java.util.Scanner;
 public class App {
-
+    public static void main(String[] args) {
+        Scanner in;
+        in = new Scanner(System.in);
+        System.out.println("Enter the text you want to encrypt:");
+        String stringLength = in.nextLine();
+    }
 }
