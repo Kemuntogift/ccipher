b05985beb979a8e5a4191781cacd5c4e3803e737
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 16:46:41 +0300

    add return statement to get the plain text

--- src/main/java/caesar/Decoding.java
+++ src/main/java/caesar/Decoding.java
@@ -1,4 +1,12 @@
 package caesar;
 
 public class Decoding {
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static String decoding(String cText, int key) {
+        cText =cText.toLowerCase();
+        //to generate cipher text that was encoded to plain text
+        String theText = "";
+        return theText;
+    }
+
 }
