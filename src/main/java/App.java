27f6f6c400870fc15a74ad7f08eb501587e0923c
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 18:02:17 +0300

    build app and test interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,19 @@ import caesar.Decoding;
 import caesar.EncodingC;
 
 import java.util.Scanner;
+import static caesar.Decoding.decoding;
+import static caesar.EncodingC.encoding;
+
 public class App {
     public static void main(String[] args) {
         Scanner in;
         in = new Scanner(System.in);
         System.out.println("Enter the text you want to encrypt:");
-        String stringLength = in.nextLine();
+        String userText = in.nextLine();
+        System.out.println("Enter encryption key between 1 and 25");
+        int Key = in.nextInt();
+
+        String inputText = encoding(userText, Key);
+        System.out.println("Your text is :" + inputText);
     }
 }
