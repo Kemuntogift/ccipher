d132313647df150bd868f838f99bc00766b84d9a
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 11:33:49 +0300

    add test method to test input of string and integer

--- src/test/java/caesar/CaesarCipherTest.java
+++ src/test/java/caesar/CaesarCipherTest.java
@@ -1,7 +1,13 @@
 package caesar;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CaesarCipherTest {
-
+public class CaesarCipherTest {
+    @Test
+    public void newSentence_containsWordsAndKey_inSentence(){
+        CaesarCipher cipherText = new CaesarCipher ("go for lunch", 5);
+        assertEquals(true,cipherText instanceof CaesarCipher);
+    }
 }
