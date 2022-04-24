b2b283efb637dc80fec9d6d489a2435c10471fd9
Kemunto Gift <giftoyugi@gmail.com>
2022-04-24 16:06:02 +0300

    generate cipher characters from the alphabet

--- src/main/java/caesar/CaesarCipher.java
+++ src/main/java/caesar/CaesarCipher.java
@@ -8,7 +8,14 @@ public class CaesarCipher {
     public CaesarCipher(String theText, int key) {
         theText = theText.toLowerCase();
         String cText = "";
-        for(int i=0; i<theText.length(); i++)
+        //loop through index of alphabet
+        for(int i=0; i<theText.length(); i++) {
+            int charIndex = alphabet.indexOf(theText.charAt(i));
+            int newIndex = (charIndex + key) % 26;
+
+            //generate cipher characters form alphabet at the new index
+            char cipherTextChar = alphabet.charAt(newIndex);
+        }
         this.theText = theText;
         this.key = key;
     }
